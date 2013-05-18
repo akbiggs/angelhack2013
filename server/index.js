f38e0406861a47f646229442dbfc0240a2e62261
akbiggs <alexander@uken.com>
2013-05-18 15:32:59 -0400

    job server complete

--- server/index.js
+++ server/index.js
@@ -1,16 +1,59 @@
 var PORT = 8888;
 
 var express = require("express");
+var fs = require("fs");
 var options = require("./options");
 
 var app = express();
+app.use(express.bodyParser());
 
 app.get("/", function(req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    console.log("root");
     redirectToMapReduce(res);
 });
 
-app.get("/map/*", function(req, res) {
-    console.log(req.query.splat);
+app.get("/map", function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    fs.readFile(req.query.file, function(err, data) {
+        if (err) {
+            console.log("ERROR: " + err);
+            res.json({error: err});
+            res.end();
+        } else {
+            res.json({map: data.toString()});
+            res.end();
+        }
+    });
+});
+
+app.get("/reduce", function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json({reduce: options.reduce_jobs});
+    options.reduce_jobs = [];
+});
+
+app.get("/done", function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json({
+        done: true,
+        result: options.result
+    });
+});
+
+app.post("/emit/:phase", function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    var data = req.body;
+    switch (req.params.phase) {
+        case "reduce": 
+            addReduceJob(parseInt(data.count));
+            res.end();
+        case "finalize": 
+            makeResult(data.sum);
+            res.end();
+    }
 });
 
 app.listen(process.env.PORT || PORT);
@@ -20,24 +63,28 @@ var redirectToMapReduce = function(res) {
         redirectToMapPage(res);
     if (moreReduceJobs())
         redirectToReducePage(res);
-    finish(res);
+    redirectToDonePage(res);
 }
 
 var redirectToMapPage = function(res) {
-    res.redirect("map/#" + options.map_jobs.pop());
+    res.redirect("map?file=" + options.map_jobs.pop());
 }
 
 var redirectToReducePage = function(res) {
-    console.log("reducing phase");
+    res.redirect("reduce");
 }
 
-var finish = function(res) {
-    redirectToDonePage(res);
-    res.end();
+var redirectToDonePage = function(res) {
+    res.redirect("done");
+}
+
+var addReduceJob = function(job) {
+    options.reduce_jobs.push(job);
 };
 
-var redirectToDonePage = function(res) {
-    console.log("done jobs");
+var makeResult = function(result) {
+    console.log("result is: " + result);
+    options.result = result;
 }
 
 var moreMapJobs = function() {
--- server/options.js
+++ server/options.js
@@ -7,11 +7,8 @@ glob("data/*.txt", function(err, files) {
     }
     
     exports.map_jobs = files;
-    /*exports.map_jobs = files.map(function(file) {
-        return file.split("/")[1];
-    });*/
     console.log("Adding map jobs: " + exports.map_jobs);
 });
 
 exports.reduce_jobs = [];
-exports.result = null;
+exports.result = 0;
